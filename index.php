858cf9c219c866c3e88bbc8178059dae4a2da162
poojadvkr <pooja@rasheedfoundation.org>
2024-05-16 09:56:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>My First PHP Page</title>
+</head>
+<body>
+ <h1>Welcome to My First PHP Page</h1>
+ <p>Hello, welcome to Pooja's World!</p>
+ <p>Current Date and Time: <?php echo date("Y-m-d H:i:s"); ?></p>
+</body>
+</html>
